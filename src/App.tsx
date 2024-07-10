95d557ed72e70ea8e062216031fbc7820eaae4aa
haivuw <haivuw@gmail.com>
2024-07-10 10:24:23 +0700

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useRef } from "react";
+import "./App.css";
+
+const decoder = new TextDecoder();
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [connected, setConnected] = React.useState(false);
+  const port = useRef<SerialPort>();
+  const reader = useRef<ReadableStreamDefaultReader<string>>();
+
+  const [messages, setMessages] = React.useState<string[]>([]);
+
+  const toggleConnection = async () => {
+    if (connected && port.current && reader.current) {
+      await reader.current.cancel();
+      reader.current.releaseLock();
+      await port.current.close();
+      port.current = undefined;
+      return setConnected(false);
+    }
+
+    const ports = await navigator.serial.getPorts();
+    if (ports.length === 0) {
+      port.current = await navigator.serial.requestPort();
+    } else {
+      port.current = ports[0];
+    }
+
+    await port.current.open({ baudRate: 9600 });
+
+    reader.current = port.current.readable.getReader();
+
+    Promise.resolve().then(async () => {
+      // Listen to data coming from the serial device.
+      while (true) {
+        const { value, done } = await reader.current!.read();
+        if (done) {
+          // Allow the serial port to be closed later.
+          reader.current!.releaseLock();
+          break;
+        }
+        const decoded = decoder.decode(value);
+        setMessages((messages) => [...messages, decoded]);
+      }
+    });
+
+    setConnected(true);
+  };
 
   return (
     <>
+      <button onClick={toggleConnection}>
+        {connected ? "Disconnect" : "Connect"}
+      </button>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Messages</h2>
+        <pre>{JSON.stringify(messages, null, 0)}</pre>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
